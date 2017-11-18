5b74a89e73936b09b1403421ca4ebf727a42d0d1
nixonka <nikola_simic@msn.com>
2017-11-18 15:37:56 +0100

    Added Angular CLI

---
+++ AspNetCoreAngularWithCLI/ClientApp/app/test-component/test-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'appc-test-component',
+  templateUrl: './test-component.component.html',
+  styleUrls: ['./test-component.component.css']
+})
+export class TestComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
